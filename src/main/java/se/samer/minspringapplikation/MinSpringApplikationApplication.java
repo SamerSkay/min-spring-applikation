e84df6b5de0cbd27585431885a369d964237564f
SamerSkay <92samska@gafe.molndal.se>
2024-09-17 11:03:00 +0200

    gjort restcontroller, samt skapat projektet

---
+++ src/main/java/se/samer/minspringapplikation/MinSpringApplikationApplication.java
@@ -0,0 +1,13 @@
+package se.samer.minspringapplikation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinSpringApplikationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinSpringApplikationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/samer/minspringapplikation/MinSpringApplikationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.samer.minspringapplikation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinSpringApplikationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
