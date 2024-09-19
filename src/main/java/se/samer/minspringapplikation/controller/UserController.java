4bd4636572b2fad44b58d0f344d9f1807462eb6f
SamerSkay <92samska@gafe.molndal.se>
2024-09-19 13:22:43 +0200

    kopplat till databasen, lagt till crud funktionaliteter för användare

---
+++ src/main/java/se/samer/minspringapplikation/controller/UserController.java
@@ -0,0 +1,38 @@
+package se.samer.minspringapplikation.controller;
+
+import org.springframework.web.bind.annotation.*;
+import se.samer.minspringapplikation.model.User;
+import se.samer.minspringapplikation.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
