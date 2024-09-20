8f008a4015d342b5e060d85b6341fc03cb0d0c33
SamerSkay <92samska@gafe.molndal.se>
2024-09-20 13:37:45 +0200

    testar actions

--- src/main/java/se/samer/minspringapplikation/controller/HelloController.java
+++ src/main/java/se/samer/minspringapplikation/controller/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
